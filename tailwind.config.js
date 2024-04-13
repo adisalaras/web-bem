10b3604805dfd41fe2e9dd60322a4d9abfcab95b
adisalaras <adisalaras41@gmail.com>
2024-04-13 15:49:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors : {
+        primary: '#018391',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        krem: '#F6DBAC',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
